af345b0b8f3ee1a64e8c4fa5cd550da4c9a9c59e
407-42-wasakit <56761@yupparaj.ac.th>
2026-06-26 11:31:57 +0700

    Update digital_profile.py

--- digital_profile.py
+++ digital_profile.py
@@ -1,1 +1,10 @@
+import streamlit as st
 
+st.title("✨My Digital Profile")
+st.header("🧩เเนะนำตัว")
+st.write("นายวสกิจ ล้ำวีรประเสริฐ")
+st.write("ชื่อเล่น ซูกัส")
+st.write("เลขที่ 42 ม.4/7")
+st.header("🎯เป้าหมายเเละสิ่งที่อยากทำในอนาคต")
+st.write("เป้าหมาย:เกรด 4.00 วิทยาการคำนวณ")
+st.write("สิ่งที่อยากเป็นในอนาคต:เเพทย์")
